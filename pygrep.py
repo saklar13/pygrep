76ff74c85c65b10b483cc6545f79677ef0915cec
Kirill <saklar13@gmail.com>
2015-07-08 17:38:08 +0300

    new

--- pygrep.py
+++ pygrep.py
@@ -4,17 +4,14 @@ from fnmatch import fnmatch
 
 def pygrep(pattern, keyword):
     for cur_dir, sub_dirs, files in os.walk('.'):
-        for file in files:
-            if fnmatch(file, pattern):
-                full_path = os.path.join(cur_dir, file)
-                with open(full_path) as f:
-                    for i, line in enumerate(f):
-                        if keyword in line:
-                            yield full_path, i, line
+        for file in (x for x in files if fnmatch(x, pattern)):
+            full_path = os.path.join(cur_dir, file)
+            with open(full_path) as f:
+                yield from ((i, line) for i, line in enumerate(f) if keyword in line)
 
 
 if __name__ == '__main__':
     import sys
     pattern, keyword = sys.argv[1:]
-    for path, i, line in pygrep(pattern, keyword):
-        print('{}\n{} => {}'.format(path, i, line))
+    for i, line in pygrep(pattern, keyword):
+        print('\n{} => {}'.format(i, line))
