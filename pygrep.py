bc4ecf7e805c8a89990a67b1116a12377387f467
Kirill <saklar13@gmail.com>
2015-07-08 17:47:59 +0300

    skips UnicodeError add file path to output

--- pygrep.py
+++ pygrep.py
@@ -5,13 +5,15 @@ from fnmatch import fnmatch
 def pygrep(pattern, keyword):
     for cur_dir, sub_dirs, files in os.walk('.'):
         for file in (x for x in files if fnmatch(x, pattern)):
-            full_path = os.path.join(cur_dir, file)
+            full_path = os.path.abspath(os.path.join(cur_dir, file))
             with open(full_path) as f:
-                yield from ((i, line) for i, line in enumerate(f) if keyword in line)
-
+                try:
+                    yield from ((i, line, full_path) for i, line in enumerate(f) if keyword in line)
+                except UnicodeDecodeError:
+                    pass
 
 if __name__ == '__main__':
     import sys
     pattern, keyword = sys.argv[1:]
-    for i, line in pygrep(pattern, keyword):
-        print('\n{} => {}'.format(i, line))
+    for i, line, path in pygrep(pattern, keyword):
+        print('{}\n{} => {}'.format(path, i, line))
